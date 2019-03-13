12bf09e7bea2e1494b2919200d239c601fcaef4b
steamCrank <darshanamarapana@gmail.com>
2019-03-13 22:08:22 +1300

    Introduce DataController, UI Controller,Global Controller Modules
    Introducing three very important modules

---
+++ main.js
@@ -0,0 +1,14 @@
+//Data Controller
+var dataController = (function(){
+
+})();
+
+//UI Controller
+var uiController = (function(){
+
+})();
+
+//Global Controller
+var globalController = (function(dataCtr, uiCtr){
+
+})(dataController, uiController);
