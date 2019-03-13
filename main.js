70eb572049fb2425c9af93536060dde3e22d5c2c
steamCrank <darshanamarapana@gmail.com>
2019-03-13 22:15:02 +1300

    Add keypress event listener
    Return key pressing event listener added.

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 //Data Controller
-var dataController = (function(){
+var dataController = (function () {
 
 })();
 
 //UI Controller
-var uiController = (function(){
+var uiController = (function () {
 
 })();
 
 //Global Controller
-var globalController = (function(dataCtr, uiCtr){
+var globalController = (function (dataCtr, uiCtr) {
+    //Add Keyboard Return Key Press Event
+    document.addEventListener('keypress', function (event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrAddItem();
+        }
 
-})(dataController, uiController);
+    })(dataController, uiController);
