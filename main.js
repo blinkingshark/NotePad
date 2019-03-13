0cc43758cdd82b75908dd6c35b7df19e08e0bd9d
steamCrank <darshanamarapana@gmail.com>
2019-03-13 22:43:56 +1300

    Input Values to Data Object
    Input values to data object, Introduce DOM Element to Global Controller

--- main.js
+++ main.js
@@ -1,19 +1,53 @@
 //Data Controller
 var dataController = (function () {
 
+    var data = {
+        allItems: []
+    };
+
+    return{
+        addData: function(value){
+            data.allItems.push(value);
+        },        
+        getData: function(){
+            console.log(data);
+        }
+    };
+
 })();
 
 //UI Controller
 var uiController = (function () {
+    var DOMStrings = {
+        inputElement: '.top__pannel__input'
+    };
+
+    return{
+        //Expose DOMElements to the public
+        getDOMElement: function(){
+            return DOMStrings;
+        }
+    }
 
 })();
 
 //Global Controller
 var globalController = (function (dataCtr, uiCtr) {
     //Add Keyboard Return Key Press Event
-    document.addEventListener('keypress', function (event) {
-        if (event.keyCode === 13 || event.which === 13) {
-            ctrAddItem();
+    document.addEventListener('keypress', function(event){
+        if(event.keyCode ===13 || event.which ===13){
+            fetchInputs(); 
         }
+    });
+    //DOM element for global controller
+    var DOM = uiCtr.getDOMElement();
 
-    })(dataController, uiController);
+    var fetchInputs = function(){
+    //Fetch the input value
+    var input = document.querySelector(DOM.inputElement).value;
+
+    //Input Data
+    dataCtr.addData(input);
+    };
+
+})(dataController, uiController);
